fe74488c900910859d0cfe9754d47d756aa2f443
shunli.wang <shunli.wang@dwfund.net>
2020-06-12 11:28:40 +0800

    ready...

---
+++ taucoin-core/src/main/java/io/taucoin/config/ChainConfig.java
@@ -0,0 +1,12 @@
+package io.taucoin.config;
+
+public class ChainConfig {
+    private static final String TauNickName="tauchain";
+    private static final String TauGenesisMinerPubkey="";
+    public String getTauNickName(){
+        return TauNickName;
+    }
+    public String getTauGenesisMinerPubkey(){
+        return TauGenesisMinerPubkey;
+    }
+}
---
+++ taucoin-core/src/main/java/io/taucoin/genesis/Genesis.java
@@ -0,0 +1,5 @@
+package io.taucoin.genesis;
+import io.taucoin.config.ChainConfig;
+
+public class Genesis {
+}
