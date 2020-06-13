a7bdfe9948ec9f8dcee82a281ca5f76e3ca8e2bd
cuitaixiang <754060645@qq.com>
2020-06-13 23:36:01 +0800

    add voting module

---
+++ taucoin-core/src/main/java/io/taucoin/core/Vote.java
@@ -0,0 +1,74 @@
+package io.taucoin.core;
+
+import java.util.Arrays;
+
+public class Vote implements Comparable<Vote> {
+    private byte[] blockHash;
+    private int blockNumber;
+    private int count;
+
+    public Vote(byte[] blockHash, int blockNumber) {
+        this.blockHash = blockHash;
+        this.blockNumber = blockNumber;
+        this.count = 0;
+    }
+
+    public Vote(byte[] blockHash, int blockNumber, int count) {
+        this.blockHash = blockHash;
+        this.blockNumber = blockNumber;
+        this.count = count;
+    }
+
+    public byte[] getBlockHash() {
+        return blockHash;
+    }
+
+    public void setBlockHash(byte[] blockHash) {
+        this.blockHash = blockHash;
+    }
+
+    public int getBlockNumber() {
+        return blockNumber;
+    }
+
+    public void setBlockNumber(int blockNumber) {
+        this.blockNumber = blockNumber;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int voteUp() {
+        this.count++;
+        return this.count;
+    }
+
+    @Override
+    // 比较策略：票数优先；票数相同的情况下，高度优先
+    public int compareTo(Vote o) {
+        // from high to low
+        // 先比较票数, 如果比较不出来，再比较高度，高度更大的优先
+        if (this.count < o.count) {
+            return 1;
+        } else if (this.count == o.count) {
+            if (this.blockNumber < o.blockNumber) {
+                return 1;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        return "Vote{" +
+                "blockHash=" + Arrays.toString(blockHash) +
+                ", blockNumber=" + blockNumber +
+                ", count=" + count +
+                '}';
+    }
+}
---
+++ taucoin-core/src/main/java/io/taucoin/core/VotingPool.java
@@ -0,0 +1,45 @@
+package io.taucoin.core;
+
+import java.util.*;
+
+public class VotingPool {
+    private Map<String, Map<String, Vote>> votingPool = new HashMap<>();
+
+//    public VotingPool(Map<String, Map<String, Vote>> votingPool) {
+//        this.votingPool = votingPool;
+//    }
+
+    public synchronized void putIntoVotingPool(String chainID, byte[] blockHash, int blockNumber) {
+        Map<String, Vote> votes = votingPool.get(chainID);
+        if(null == votes) {
+            votes = new HashMap<>();
+        }
+        String key = new String(blockHash);
+        Vote vote = votes.get(key);
+        if (null == vote) {
+            vote = new Vote(blockHash, blockNumber);
+        }
+        vote.voteUp();
+
+        votes.put(key, vote);
+        votingPool.put(chainID, votes);
+    }
+
+    public synchronized void removeAllVotes(String chainID) {
+        if(votingPool.containsKey(chainID)) {
+            votingPool.remove(chainID);
+        }
+    }
+
+    public synchronized List getSortedVotes(String chainID) {
+        Map<String, Vote> votes = votingPool.get(chainID);
+        if(null == votes || votes.isEmpty()) {
+            return null;
+        }
+
+        List<Vote> list = new ArrayList<>(votes.values());
+        Collections.sort(list);
+
+        return list;
+    }
+}
