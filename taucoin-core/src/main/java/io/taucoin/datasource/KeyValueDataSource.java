81cdbeae4d7c4d5e732a1d4bed46c3ebf2df1434
cuitaixiang <754060645@qq.com>
2020-06-18 23:42:48 +0800

    add db interface and leveldb implementation

---
+++ taucoin-core/src/main/java/io/taucoin/datasource/KeyValueDataSource.java
@@ -0,0 +1,14 @@
+package io.taucoin.datasource;
+
+import java.util.Map;
+
+public interface KeyValueDataSource extends DataSource {
+
+    byte[] get(byte[] key);
+
+    byte[] put(byte[] key, byte[] value);
+
+    void delete(byte[] key);
+
+    void updateBatch(Map<byte[], byte[]> rows);
+}
