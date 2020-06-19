cadde22e41e18819327d7ce58a32b719ca460509
cuitaixiang <754060645@qq.com>
2020-06-19 22:47:41 +0800

    add account state

---
+++ taucoin-core/src/main/java/io/taucoin/core/AccountState.java
@@ -0,0 +1,57 @@
+package io.taucoin.core;
+
+import io.taucoin.util.RLP;
+import io.taucoin.util.RLPList;
+
+import java.math.BigInteger;
+
+public class AccountState {
+    private BigInteger balance;
+    private BigInteger nonce;
+
+    public AccountState(byte[] rlp) {
+        RLPList decodedTxList = RLP.decode2(rlp);
+        RLPList accountState = (RLPList) decodedTxList.get(0);
+        byte[] balanceBytes = accountState.get(0).getRLPData();
+        this.balance = balanceBytes == null ? BigInteger.ZERO :
+                new BigInteger(1, balanceBytes);
+        byte[] nonceBytes = accountState.get(0).getRLPData();
+        this.nonce = nonceBytes == null ? BigInteger.ZERO :
+                new BigInteger(1, nonceBytes);
+    }
+
+    public AccountState(BigInteger balance, BigInteger nonce) {
+        this.balance = balance;
+        this.nonce = nonce;
+    }
+
+    public BigInteger getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigInteger balance) {
+        this.balance = balance;
+    }
+
+    public BigInteger getNonce() {
+        return nonce;
+    }
+
+    public void setNonce(BigInteger nonce) {
+        this.nonce = nonce;
+    }
+
+    public byte[] getEncoded() {
+        byte[] balance = RLP.encodeBigInteger(this.balance);
+        byte[] nonce = RLP.encodeBigInteger(this.nonce);
+        return RLP.encodeList(balance, nonce);
+    }
+
+    @Override
+    public String toString() {
+        return "AccountState{" +
+                "balance=" + balance +
+                ", nonce=" + nonce +
+                '}';
+    }
+}
