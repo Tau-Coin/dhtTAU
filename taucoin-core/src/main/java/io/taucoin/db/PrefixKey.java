4f651e5b29a0d258a3fe7ab3f25921d7186f3949
cuitaixiang <754060645@qq.com>
2020-06-23 22:15:22 +0800

    compose prefix and key in db

---
+++ taucoin-core/src/main/java/io/taucoin/db/PrefixKey.java
@@ -0,0 +1,150 @@
+package io.taucoin.db;
+
+public class PrefixKey {
+    private static final byte[] chain = "Chain".getBytes();
+    private static final byte[] bestBlock = "BestBlock".getBytes();
+    private static final byte[] mutableRange = "MutableRange".getBytes();
+    private static final byte[] immutablePointBlockHash = "ImmutablePointBlockHash".getBytes();
+    private static final byte[] votesCountingPointBlockHash = "VotesCountingPointBlockHash".getBytes();
+    private static final byte[] peer = "Peer".getBytes();
+    private static final byte[] txPool = "TxPool".getBytes();
+
+    /**
+     * chain key: "Chain" + chainID
+     * @param chainID
+     * @return
+     */
+    public static byte[] chainKey(byte[] chainID) {
+        byte[] key = new byte[chain.length + chainID.length];
+        System.arraycopy(chain, 0, key, 0, chain.length);
+        System.arraycopy(chainID, 0, key, chain.length, chainID.length);
+        return key;
+    }
+
+    /**
+     * best block hash key: chainID + "BestBlock"
+     * @param chainID
+     * @return
+     */
+    public static byte[] bestBlockHashKey(byte[] chainID) {
+        byte[] key = new byte[chainID.length + bestBlock.length];
+        System.arraycopy(chainID, 0, key, 0, chainID.length);
+        System.arraycopy(bestBlock, 0, key, chainID.length, bestBlock.length);
+        return key;
+    }
+
+    /**
+     * immutable point block hash key: chainID + "ImmutablePointBlockHash"
+     * @param chainID
+     * @return
+     */
+    public static byte[] immutablePointBlockHashKey(byte[] chainID) {
+        byte[] key = new byte[chainID.length + immutablePointBlockHash.length];
+        System.arraycopy(chainID, 0, key, 0, chainID.length);
+        System.arraycopy(immutablePointBlockHash, 0, key, chainID.length, immutablePointBlockHash.length);
+        return key;
+    }
+
+    /**
+     * votes counting point block hash key: chainID + "VotesCountingPointBlockHash"
+     * @param chainID
+     * @return
+     */
+    public static byte[] votesCountingPointBlockHashKey(byte[] chainID) {
+        byte[] key = new byte[chainID.length + votesCountingPointBlockHash.length];
+        System.arraycopy(chainID, 0, key, 0, chainID.length);
+        System.arraycopy(votesCountingPointBlockHash, 0, key, chainID.length, votesCountingPointBlockHash.length);
+        return key;
+    }
+
+    /**
+     * mutable range key: chainID + "MutableRange"
+     * @param chainID
+     * @return
+     */
+    public static byte[] mutableRangeKey(byte[] chainID) {
+        byte[] key = new byte[chainID.length + mutableRange.length];
+        System.arraycopy(chainID, 0, key, 0, chainID.length);
+        System.arraycopy(mutableRange, 0, key, chainID.length, mutableRange.length);
+        return key;
+    }
+
+    /**
+     * peer key: chainID + "Peer"
+     * @param chainID
+     * @return
+     */
+    public static byte[] peerKeyPrefix(byte[] chainID) {
+        byte[] prefix = new byte[chainID.length + peer.length];
+        System.arraycopy(chainID, 0, prefix, 0, chainID.length);
+        System.arraycopy(peer, 0, prefix, chainID.length, peer.length);
+        return prefix;
+    }
+
+    /**
+     * peer key: chainID + "Peer" + peer pubkey
+     * @param chainID
+     * @param pubkey
+     * @return
+     */
+    public static byte[] peerKey(byte[] chainID, byte[] pubkey) {
+        byte[] key = new byte[chainID.length + peer.length + pubkey.length];
+        System.arraycopy(chainID, 0, key, 0, chainID.length);
+        System.arraycopy(peer, 0, key, chainID.length, peer.length);
+        System.arraycopy(pubkey, 0, key, chainID.length + peer.length, pubkey.length);
+        return key;
+    }
+
+    /**
+     * tx pool prefix: chainID + "TxPool"
+     * @param chainID
+     * @return
+     */
+    public static byte[] txPoolPrefix(byte[] chainID) {
+        byte[] prefix = new byte[chainID.length + txPool.length];
+        System.arraycopy(chainID, 0, prefix, 0, chainID.length);
+        System.arraycopy(txPool, 0, prefix, chainID.length, txPool.length);
+        return prefix;
+    }
+
+    /**
+     * tx pool key: chainID + "TxPool" + txid
+     * @param chainID
+     * @param txid
+     * @return
+     */
+    public static byte[] txPoolKey(byte[] chainID, byte[] txid) {
+        byte[] key = new byte[chainID.length + txPool.length + txid.length];
+        System.arraycopy(chainID, 0, key, 0, chainID.length);
+        System.arraycopy(txPool, 0, key, chainID.length, txPool.length);
+        System.arraycopy(txid, 0, key, chainID.length + txPool.length, txid.length);
+        return key;
+    }
+
+    /**
+     * account key: chainID + pubkey
+     * @param chainID
+     * @param pubkey
+     * @return
+     */
+    public static byte[] accountKey(byte[] chainID, byte[] pubkey) {
+        byte[] key = new byte[chainID.length + pubkey.length];
+        System.arraycopy(chainID, 0, key, 0, chainID.length);
+        System.arraycopy(pubkey, 0, key, chainID.length, pubkey.length);
+        return key;
+    }
+
+    /**
+     * block key: chainID + hash
+     * @param chainID
+     * @param hash
+     * @return
+     */
+    public static byte[] blockKey(byte[] chainID, byte[] hash) {
+        byte[] key = new byte[chainID.length + hash.length];
+        System.arraycopy(chainID, 0, key, 0, chainID.length);
+        System.arraycopy(hash, 0, key, chainID.length, hash.length);
+        return key;
+    }
+
+}
--- taucoin-core/src/main/java/io/taucoin/db/StateDB.java
+++ taucoin-core/src/main/java/io/taucoin/db/StateDB.java
@@ -1,10 +1,140 @@
 package io.taucoin.db;
 
-public class StateDB {
+import io.taucoin.core.AccountState;
+import io.taucoin.types.Transaction;
+
+import java.util.Set;
+
+public class StateDB implements Repository{
 
     private KeyValueDataBase db;
 
     public StateDB(KeyValueDataBase db) {
         this.db = db;
+    }
+
+    @Override
+    public void followChain(byte[] chainID) {
+
+    }
+
+    @Override
+    public Set<byte[]> getChains() {
+        return null;
+    }
+
+    @Override
+    public void deleteChain(byte[] chainID) {
+
+    }
+
+    @Override
+    public void setBestBlockHash(byte[] chainID, byte[] hash) {
+
+    }
+
+    @Override
+    public byte[] getBestBlockHash(byte[] chainID) {
+        return new byte[0];
+    }
+
+    @Override
+    public void deleteBestBlockHash(byte[] chainID) {
+
+    }
+
+    @Override
+    public void setMutableRange(byte[] chainID, int number) {
+
+    }
+
+    @Override
+    public int getMutableRange(byte[] chainID) {
+        return 0;
+    }
+
+    @Override
+    public void deleteMutableRange(byte[] chainID) {
+
+    }
+
+    @Override
+    public void addPeer(byte[] chainID, byte[] pubkey) {
+
+    }
+
+    @Override
+    public Set<byte[]> getPeers(byte[] chainID) {
+        return null;
+    }
+
+    @Override
+    public void deletePeer(byte[] chainID, byte[] pubkey) {
+
+    }
+
+    @Override
+    public void deleteAllPeers(byte[] chainID) {
+
+    }
+
+    @Override
+    public Set<Transaction> getTAUSelfTxPool(byte[] chainID) {
+        return null;
+    }
+
+    @Override
+    public void updateTAUSelfTxPool(byte[] chainID, Set<Transaction> txs) {
+
+    }
+
+    @Override
+    public void deleteTAUSelfTxPool(byte[] chainID) {
+
+    }
+
+    @Override
+    public void setImmutablePointBlockHash(byte[] chainID, byte[] hash) {
+
+    }
+
+    @Override
+    public byte[] getImmutablePointBlockHash(byte[] chainID) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] deleteImmutablePointBlockHash(byte[] chainID) {
+        return new byte[0];
+    }
+
+    @Override
+    public void setVotesCountingPointBlockHash(byte[] chainID, byte[] hash) {
+
+    }
+
+    @Override
+    public byte[] getVotesCountingPointBlockHash(byte[] chainID) {
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] deleteVotesCountingPointBlockHash(byte[] chainID) {
+        return new byte[0];
+    }
+
+    @Override
+    public void updateAccounts(Set<AccountState> accountStateSet) {
+
+    }
+
+    @Override
+    public AccountState getAccount(byte[] chainID, byte[] pubkey) {
+        return null;
+    }
+
+    @Override
+    public void deleteAccount(byte[] chainID, byte[] pubkey) {
+
     }
 }
