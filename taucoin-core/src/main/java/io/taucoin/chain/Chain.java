72aa74adf5d8df1a6b7473edcda3f151d6c1677a
cuitaixiang <754060645@qq.com>
2020-07-11 23:38:01 +0800

    add func to chain

--- taucoin-core/src/main/java/io/taucoin/chain/Chain.java
+++ taucoin-core/src/main/java/io/taucoin/chain/Chain.java
@@ -1,6 +1,16 @@
 package io.taucoin.chain;
 
+import io.taucoin.account.AccountManager;
+import io.taucoin.core.ProofOfTransaction;
+import io.taucoin.core.TransactionPool;
+import io.taucoin.core.VotingPool;
+import io.taucoin.db.BlockStore;
+import io.taucoin.db.Repository;
 import io.taucoin.listener.TauListener;
+import io.taucoin.types.Block;
+import io.taucoin.types.Transaction;
+
+import java.math.BigInteger;
 
 /**
  * Chain represents one blockchain for tau multi-chain system.
@@ -19,6 +29,22 @@ public class Chain {
 
     private TauListener tauListener;
 
+    private AccountManager accountManager;
+
+    private ProofOfTransaction pot;
+
+    private TransactionPool txPool;
+
+    private VotingPool votingPool;
+
+    private BlockStore blockStore;
+
+    private Repository repository;
+
+    private byte[] genesisBlockHash;
+
+    private Block bestBlock;
+
     /**
      * Chain constructor.
      *
@@ -27,6 +53,25 @@ public class Chain {
     public Chain(byte[] chainID, TauListener tauListener) {
         this.chainID = chainID;
         this.tauListener = tauListener;
+    }
+
+    private void init() {
+
+    }
+
+    private void processBlock(Block block) {
+
+    }
+
+    private Block mine() {
+        Transaction tx = txPool.getBestTransaction();
+        BigInteger baseTarget = pot.calculateRequiredBaseTarget(this.bestBlock, this.blockStore);
+        byte[] generationSignature = pot.calculateGenerationSignature(this.bestBlock.getGenerationSignature(),
+                accountManager.getPublickey());
+        BigInteger cumulativeDifficulty = pot.calculateCumulativeDifficulty(this.bestBlock.getCumulativeDifficulty(),
+                baseTarget);
+        byte[] immutableBlockHash;
+        return null;
     }
 
     /**
