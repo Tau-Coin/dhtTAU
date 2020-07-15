d873d268744841a81d0ba3cb1e053f6437482595
shengxianshui <shengxianshui@imorpheusai.io>
2020-07-15 11:05:17 +0800

    Commit json rpc module

---
+++ taucoin-linux/src/main/java/io/taucoin/jtau/rpc/JsonRpcServer.java
@@ -0,0 +1,104 @@
+package io.taucoin.jtau.rpc;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+import com.thetransactioncompany.jsonrpc2.server.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.taucoin.controller.TauController;
+import io.taucoin.jtau.rpc.method.*;
+
+import java.net.InetAddress;
+
+/**
+ * Taucoin blockchain json rpc server.
+ * You can use any json rpc client to access this service.
+ */
+public final class JsonRpcServer {
+
+    private static final Logger logger = LoggerFactory.getLogger("rpc");
+
+    // TauController through which all blockchain components can be accessed.
+    private TauController tauController;
+
+    // method dispatcher.
+    private Dispatcher dispatcher;
+
+    // netty evnent loop group.
+    EventLoopGroup bossGroup;
+    EventLoopGroup workerGroup;
+
+    // http server listening port.
+    private int port;
+
+    /**
+     * JsonRpcServer constructor.
+     *
+     * @param controller TauController
+     */
+    public JsonRpcServer(TauController controller) {
+        this.tauController = controller;
+
+        this.dispatcher = new Dispatcher();
+
+        // register all rpc methods.
+        // methods about dht
+        this.dispatcher.register(new dht_nodesCount(this.tauController));
+    }
+
+    /**
+     * Start http json rcp server.
+     *
+     * @param port http server listening port
+     */
+    public void start(int port) throws Exception {
+        bossGroup = new NioEventLoopGroup(1);
+        workerGroup = new NioEventLoopGroup();
+        this.port = port;
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.option(ChannelOption.SO_BACKLOG, 1024);
+            b.localAddress(port);
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new JsonRpcServerInitializer());
+
+            Channel ch = b.bind().sync().channel();
+
+            logger.info("Json rpc server is starting, listen port: {}", this.port);
+
+            ch.closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    /**
+     * Stop http json rcp server.
+     */
+    public void stop() {
+        if (bossGroup != null) {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    class JsonRpcServerInitializer extends ChannelInitializer<SocketChannel> {
+        @Override
+        public void initChannel(SocketChannel ch) {
+            ChannelPipeline p = ch.pipeline();
+            p.addLast(new HttpServerCodec());
+            p.addLast(new JsonRpcServerHandler(dispatcher));
+        }
+    }
+}
---
+++ taucoin-linux/src/main/java/io/taucoin/jtau/rpc/JsonRpcServerMethod.java
@@ -0,0 +1,60 @@
+package io.taucoin.jtau.rpc;
+
+import com.thetransactioncompany.jsonrpc2.*;
+import com.thetransactioncompany.jsonrpc2.server.*;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.taucoin.controller.TauController;
+
+import java.math.BigInteger;
+
+/**
+ * Method handler for json rpc.
+ */
+public abstract class JsonRpcServerMethod implements RequestHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger("rpc");
+
+    // Method name which is the class simple name.
+    private String name = "";
+
+    // TauController through which all blockchain components can be accessed.
+    protected TauController tauController;
+
+    /**
+     * JsonRpcServerMethod constrctor.
+     *
+     * @param tauController TauController
+     */
+    public JsonRpcServerMethod(TauController tauController) {
+        this.tauController = tauController;
+        this.name = this.getClass().getSimpleName();
+    }
+
+    /**
+     * Indicate which requests this method handles.
+     */
+    public String[] handledRequests() {
+        return new String[]{name};
+    }
+
+    /**
+     * Process the corresponding rpc reqeust.
+     *
+     * @param req json rpc request.
+     * @param ctx request context
+     * @return JSONRPC2Response
+     */
+    public JSONRPC2Response process(JSONRPC2Request req, MessageContext ctx) {
+        if (req.getMethod().equals(name)) {
+            return worker(req, ctx);
+        } else {
+            return new JSONRPC2Response(JSONRPC2Error.METHOD_NOT_FOUND, req.getID());
+        }
+    }
+
+    protected abstract JSONRPC2Response worker(JSONRPC2Request req, MessageContext ctx);
+}
