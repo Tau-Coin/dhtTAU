35a45dfdcf6eb4748641a60da787541ae4565e8e
hankelc <taichen.liu@imorpheus.io>
2020-10-26 08:46:52 +0800

    modify for genesis block creating

--- taucoin-core/src/main/java/io/taucoin/genesis/TauGenesisConfig.java
+++ taucoin-core/src/main/java/io/taucoin/genesis/TauGenesisConfig.java
@@ -15,10 +15,6 @@ public final class TauGenesisConfig extends GenesisConfig {
     // TAU genesis block timestamp
     public static final long TimeStamp = 1597735687;
 
-    // TAU genesis tx signature
-    public static final String Signature
-            = "50938c31196ba35a8a822a14e2c9887b05b592828cea3263c75e0dd796f78ca086f3501f36420f7ce37e16c336478da2b3b69e903d54d0054182ce257b6c7c0a";
-
    // This private key is just for test to generate signature.
     private static final String sPrivKey
             = "f008065e3ff567d4471231a4a0609e118b28f0639f9768d3f8bb123f8f0b38706ade0527cb0dd1e57ad0003fbf8e5af51c0bf0471e639b4920ab49ac17ff88f1";
@@ -44,22 +40,18 @@ public final class TauGenesisConfig extends GenesisConfig {
 
     private TauGenesisConfig() {
 
-        /*
-        super(1L, TimeStamp, GenesisConfig.DefaultBaseTarget,
-                GenesisConfig.DefaultCummulativeDifficulty,
-                Hex.decode(PubKey), Hex.decode(Signature),
-                TauGenesisTransaction.getInstance());
-         */
-
         // The following code is used to generate block signature
 
         super(1L, TimeStamp, GenesisConfig.DefaultBaseTarget,
                 GenesisConfig.DefaultCummulativeDifficulty,
                 Hex.decode(PubKey), null,
                 TauGenesisTransaction.getInstance());
+
         this.genesisBlock.signBlock(Hex.decode(sPrivKey));
+
         logger.info("blk signature:" + Hex.toHexString(this.genesisBlock.getSignature()));
 
-        //logger.info("verify signature result:" + this.genesisBlock.verifyBlockSig());
+        logger.info("verify signature result:" + this.genesisBlock.verifyBlockSig());
     }
+
 }
