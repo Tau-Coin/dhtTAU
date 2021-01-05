435faafc0f5f003eac5717578bb4adf5b112edf9
cuitaixiang <754060645@qq.com>
2021-01-05 11:29:38 +0800

    add skipMessageRoot to Message

--- taucoin-core/src/main/java/io/taucoin/types/Message.java
+++ taucoin-core/src/main/java/io/taucoin/types/Message.java
@@ -10,30 +10,36 @@ import io.taucoin.util.RLP;
 import io.taucoin.util.RLPList;
 
 public class Message {
-    private MessageVersion version;
+    private MessageVersion version; // 标识消息版本
     private BigInteger timestamp;
-    private byte[] previousMsgDAGRoot;
-    private byte[] friendLatestMessageRoot;
-    private MessageType type;
-    private byte[] content;
+    private byte[] previousMsgDAGRoot; // 对应horizontal概念
+    private byte[] friendLatestMessageRoot; // 对应horizontal概念
+    // 利用skip list概念来提升访问效率和处理访问失败的备份方案，本质上是vertical的概念
+    private byte[] skipMessageRoot; // 指向前一个完整意义的消息
+    private MessageType type;  // 可以标识消息类型
+    private byte[] content; // 消息体
 
-    private byte[] hash;
+    private byte[] hash; // gossip hash 入口
     private byte[] encode;
     private boolean parsed = false;
 
-    public static Message CreateTextMessage(BigInteger timestamp, byte[] previousMsgDAGRoot, byte[] friendLatestMessageRoot, byte[] content) {
-        return new Message(MessageVersion.VERSION1, timestamp, previousMsgDAGRoot, friendLatestMessageRoot, MessageType.TEXT, content);
+    public static Message CreateTextMessage(BigInteger timestamp, byte[] previousMsgDAGRoot,
+                                            byte[] friendLatestMessageRoot, byte[] skipMessageRoot, byte[] content) {
+        return new Message(MessageVersion.VERSION1, timestamp, previousMsgDAGRoot,
+                friendLatestMessageRoot, skipMessageRoot, MessageType.TEXT, content);
     }
 
 //    public static Message CreatePictureMessage(BigInteger timestamp, byte[] previousMsgDAGRoot, byte[] friendLatestMessageRoot, byte[] contentLink) {
 //        return new Message(MessageVersion.VERSION1, timestamp, previousMsgDAGRoot, friendLatestMessageRoot, MessageType.PICTURE, contentLink);
 //    }
 
-    public Message(MessageVersion version, BigInteger timestamp, byte[] previousMsgDAGRoot, byte[] friendLatestMessageRoot, MessageType type, byte[] content) {
+    public Message(MessageVersion version, BigInteger timestamp, byte[] previousMsgDAGRoot,
+                   byte[] friendLatestMessageRoot, byte[] skipMessageRoot, MessageType type, byte[] content) {
         this.version = version;
         this.timestamp = timestamp;
         this.previousMsgDAGRoot = previousMsgDAGRoot;
         this.friendLatestMessageRoot = friendLatestMessageRoot;
+        this.skipMessageRoot = skipMessageRoot;
         this.type = type;
         this.content = content;
 
@@ -74,6 +80,14 @@ public class Message {
         }
 
         return friendLatestMessageRoot;
+    }
+
+    public byte[] getSkipMessageRoot() {
+        if (!this.parsed) {
+            parseRLP();
+        }
+
+        return skipMessageRoot;
     }
 
     public MessageType getType() {
@@ -119,7 +133,9 @@ public class Message {
 
         this.friendLatestMessageRoot = messageList.get(3).getRLPData();
 
-        byte[] typeBytes = messageList.get(4).getRLPData();
+        this.skipMessageRoot = messageList.get(4).getRLPData();
+
+        byte[] typeBytes = messageList.get(5).getRLPData();
         int typeNum = null == typeBytes ? 0: new BigInteger(1, typeBytes).intValue();
         if (typeNum >= MessageType.UNKNOWN.ordinal()) {
             this.type = MessageType.UNKNOWN;
@@ -127,7 +143,7 @@ public class Message {
             this.type = MessageType.values()[typeNum];
         }
 
-        this.content = messageList.get(5).getRLPData();
+        this.content = messageList.get(6).getRLPData();
 
         this.parsed = true;
     }
@@ -138,10 +154,11 @@ public class Message {
             byte[] timestamp = RLP.encodeBigInteger(this.timestamp);
             byte[] previousMsgDAGRoot = RLP.encodeElement(this.previousMsgDAGRoot);
             byte[] friendLatestMessageRoot = RLP.encodeElement(this.friendLatestMessageRoot);
+            byte[] skipMessageRoot = RLP.encodeElement(this.skipMessageRoot);
             byte[] type = RLP.encodeBigInteger(BigInteger.valueOf(this.type.ordinal()));
             byte[] content = RLP.encodeElement(this.content);
 
-            this.encode = RLP.encodeList(version, timestamp, previousMsgDAGRoot, friendLatestMessageRoot, type, content);
+            this.encode = RLP.encodeList(version, timestamp, previousMsgDAGRoot, friendLatestMessageRoot, skipMessageRoot, type, content);
         }
 
         return this.encode;
@@ -149,18 +166,54 @@ public class Message {
 
     @Override
     public String toString() {
+        MessageVersion version = getVersion();
+        BigInteger timestamp = getTimestamp();
         byte[] previousRoot = getPreviousMsgDAGRoot();
         byte[] friendRoot = getFriendLatestMessageRoot();
+        byte[] skipRoot = getSkipMessageRoot();
+        MessageType type = getType();
         byte[] content = getContent();
+        byte[] hash = getHash();
 
-        return "Message{" +
-                "version=" + getVersion() +
-                ", timestamp=" + getTimestamp() +
-                ", previousMsgDAGRoot=" + (null != previousRoot ? Hex.toHexString(previousRoot) : " ") +
-                ", friendLatestMessageRoot=" + (null != friendRoot ? Hex.toHexString(friendRoot) : " ") +
-                ", type=" + getType() +
-                ", content=" + (null != content ? new String(content) : " ") +
-                ", hash=" + Hex.toHexString(getHash()) +
-                '}';
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("GossipItem{");
+
+        if (null != hash) {
+            stringBuilder.append("hash=");
+            stringBuilder.append(Hex.toHexString(hash));
+        }
+        if (null != version) {
+            stringBuilder.append(", version=");
+            stringBuilder.append(version);
+        }
+        if (null != timestamp) {
+            stringBuilder.append(", timestamp=");
+            stringBuilder.append(timestamp);
+        }
+        if (null != previousRoot) {
+            stringBuilder.append("previousMsgRoot=");
+            stringBuilder.append(Hex.toHexString(previousRoot));
+        }
+        if (null != friendRoot) {
+            stringBuilder.append("friendMsgRoot=");
+            stringBuilder.append(Hex.toHexString(friendRoot));
+        }
+        if (null != skipRoot) {
+            stringBuilder.append("skipMsgRoot=");
+            stringBuilder.append(Hex.toHexString(skipRoot));
+        }
+        if (null != type) {
+            stringBuilder.append(", type=");
+            stringBuilder.append(type);
+        }
+        if (null != content) {
+            stringBuilder.append(", content=");
+            stringBuilder.append(new String(content));
+        }
+
+        stringBuilder.append("}");
+
+        return stringBuilder.toString();
     }
 }
