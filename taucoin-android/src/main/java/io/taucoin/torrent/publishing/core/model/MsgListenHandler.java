7d47a98201287e5204e565d1b4f98c1dafa25c89
yang <1126923037@qq.com>
2021-03-11 17:38:43 +0800

    Multithreading to save data

--- taucoin-android/src/main/java/io/taucoin/torrent/publishing/core/model/MsgListenHandler.java
+++ taucoin-android/src/main/java/io/taucoin/torrent/publishing/core/model/MsgListenHandler.java
@@ -8,6 +8,11 @@ import org.slf4j.LoggerFactory;
 
 import java.math.BigInteger;
 
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 import io.taucoin.listener.MsgStatus;
 import io.taucoin.torrent.publishing.MainApplication;
 import io.taucoin.torrent.publishing.core.model.data.ChatMsgStatus;
@@ -24,8 +29,10 @@ import io.taucoin.torrent.publishing.core.storage.sqlite.repo.DeviceRepository;
 import io.taucoin.torrent.publishing.core.storage.sqlite.repo.FriendRepository;
 import io.taucoin.torrent.publishing.core.storage.sqlite.repo.UserRepository;
 import io.taucoin.torrent.publishing.core.utils.DateUtil;
+import io.taucoin.torrent.publishing.core.utils.MsgSplitUtil;
 import io.taucoin.torrent.publishing.core.utils.StringUtil;
 import io.taucoin.torrent.publishing.core.utils.UsersUtil;
+import io.taucoin.torrent.publishing.core.utils.Utils;
 import io.taucoin.types.Message;
 import io.taucoin.util.ByteUtil;
 
@@ -34,6 +41,7 @@ import io.taucoin.util.ByteUtil;
  */
 class MsgListenHandler extends MsgListener{
     private static final Logger logger = LoggerFactory.getLogger("MsgListenHandler");
+    private CompositeDisposable disposables = new CompositeDisposable();
     private ChatRepository chatRepo;
     private FriendRepository friendRepo;
     private CommunityRepository communityRepo;
@@ -57,54 +65,67 @@ class MsgListenHandler extends MsgListener{
      */
     @Override
     public void onNewMessage(byte[] friendPk, Message message) {
-        try {
-            // 朋友默认为发送者
-            String senderPk = ByteUtil.toHexString(message.getSender());
-            String receiverPk = ByteUtil.toHexString(message.getReceiver());
-            String hash = ByteUtil.toHexString(message.getHash());
-            String logicMsgHash = ByteUtil.toHexString(message.getLogicMsgHash());
+        Disposable disposable = Flowable.create(emitter -> {
+            try {
+                // 朋友默认为发送者
+                String senderPk = ByteUtil.toHexString(message.getSender());
+                String receiverPk = ByteUtil.toHexString(message.getReceiver());
+                String hash = ByteUtil.toHexString(message.getHash());
+                String logicMsgHash = ByteUtil.toHexString(message.getLogicMsgHash());
 
-            long sentTime = message.getTimestamp().longValue();
-            long receivedTime = DateUtil.getTime();
+                long sentTime = message.getTimestamp().longValue();
+                long receivedTime = DateUtil.getTime();
 
-            ChatMsg chatMsg = chatRepo.queryChatMsg(senderPk, hash);
-            logger.debug("TAU messaging onNewMessage senderPk::{}, receiverPk::{}, hash::{}, " +
-                            "SentTime::{}, ReceivedTime::{}, DelayTime::{}s, exist::{}",
-                    senderPk, receiverPk, hash,
-                    DateUtil.formatTime(sentTime, DateUtil.pattern6),
-                    DateUtil.formatTime(receivedTime, DateUtil.pattern6),
-                    receivedTime - sentTime, chatMsg != null);
-            // 上报的Message有可能重复, 如果本地已存在不处理
-            if (null == chatMsg) {
-                // 处理ChatName，如果为空，取显朋友显示名
-                String communityName = UsersUtil.getDefaultName(senderPk);
-                Community community = communityRepo.getChatByFriendPk(senderPk);
-                if (null == community) {
-                    community = new Community(senderPk, communityName);
-                    community.type = 1;
-                    community.publicKey = receiverPk;
-                    communityRepo.addCommunity(community);
-                }
-                // 更新朋友信息
-                Friend friend = friendRepo.queryFriend(receiverPk, senderPk);
-                if (friend != null) {
-                    friend.state = 2;
-                    friend.lastSeenTime = DateUtil.getTime();
-                    BigInteger currentCommTime = BigInteger.valueOf(sentTime);
-                    BigInteger lastCommTime = BigInteger.valueOf(friend.lastCommTime);
-                    if (currentCommTime.compareTo(lastCommTime) > 0) {
-                        friend.lastCommTime = sentTime;
+                ChatMsg chatMsg = chatRepo.queryChatMsg(senderPk, hash);
+                logger.debug("TAU messaging onNewMessage senderPk::{}, receiverPk::{}, hash::{}, " +
+                                "SentTime::{}, ReceivedTime::{}, DelayTime::{}s, exist::{}",
+                        senderPk, receiverPk, hash,
+                        DateUtil.formatTime(sentTime, DateUtil.pattern6),
+                        DateUtil.formatTime(receivedTime, DateUtil.pattern6),
+                        receivedTime - sentTime, chatMsg != null);
+                // 上报的Message有可能重复, 如果本地已存在不处理
+                if (null == chatMsg) {
+                    // 处理ChatName，如果为空，取显朋友显示名
+                    String communityName = UsersUtil.getDefaultName(senderPk);
+                    Community community = communityRepo.getChatByFriendPk(senderPk);
+                    if (null == community) {
+                        community = new Community(senderPk, communityName);
+                        community.type = 1;
+                        community.publicKey = receiverPk;
+                        communityRepo.addCommunity(community);
                     }
-                    friendRepo.updateFriend(friend);
+                    // 更新朋友信息
+                    Friend friend = friendRepo.queryFriend(receiverPk, senderPk);
+                    if (friend != null) {
+                        friend.state = 2;
+                        friend.lastSeenTime = DateUtil.getTime();
+                        BigInteger currentCommTime = BigInteger.valueOf(sentTime);
+                        BigInteger lastCommTime = BigInteger.valueOf(friend.lastCommTime);
+                        if (currentCommTime.compareTo(lastCommTime) > 0) {
+                            friend.lastCommTime = sentTime;
+                        }
+                        friendRepo.updateFriend(friend);
+                    }
+
+                    User user = userRepo.getCurrentUser();
+                    // 原始数据解密
+                    byte[] cryptoKey = Utils.keyExchange(senderPk, user.seed);
+                    message.decrypt(cryptoKey);
+
+                    String content = MsgSplitUtil.textBytesToString(message.getRawContent());
+                    ChatMsg msg = new ChatMsg(hash, senderPk, receiverPk, content, message.getType().ordinal(),
+                            sentTime, message.getNonce().longValue(), logicMsgHash);
+                    msg.unsent = 1;
+                    chatRepo.addChatMsg(msg);
                 }
-                ChatMsg msg = new ChatMsg(hash, senderPk, receiverPk, message.getType().ordinal(),
-                        sentTime, message.getNonce().longValue(), logicMsgHash);
-                msg.unsent = 1;
-                chatRepo.addChatMsg(msg);
+            } catch (Exception e) {
+                logger.error("onNewMessage error", e);
             }
-        } catch (Exception e) {
-            logger.error("onNewMessage error", e);
-        }
+            emitter.onComplete();
+        }, BackpressureStrategy.LATEST)
+                .subscribeOn(Schedulers.io())
+                .subscribe();
+        disposables.add(disposable);
     }
 
     /**
@@ -114,21 +135,27 @@ class MsgListenHandler extends MsgListener{
      */
     @Override
     public void onReadMessageRoot(byte[] friendPk, byte[] root) {
-        try {
-            String hash = ByteUtil.toHexString(root);
-            String friendPkStr = ByteUtil.toHexString(friendPk);
-            logger.trace("onReadMessageRoot friendPk::{}，MessageRoot::{}",
-                    friendPkStr, hash);
-            String userPk = MainApplication.getInstance().getPublicKey();
-            ChatMsgLog msgLog = new ChatMsgLog(hash, userPk, friendPkStr,
-                    ChatMsgStatus.RECEIVED_CONFIRMATION.getStatus(), DateUtil.getMillisTime());
-            long result = chatRepo.addChatMsgLog(msgLog);
-            logger.trace("updateReceivedConfirmationState friendPk::{}, msgRoot::{}, result::{}",
-                    friendPkStr, hash, result);
-        } catch (SQLiteConstraintException ignore) {
-        } catch (Exception e) {
-            logger.error("onReadMessageRoot error", e);
-        }
+        Disposable disposable = Flowable.create(emitter -> {
+            try {
+                String hash = ByteUtil.toHexString(root);
+                String friendPkStr = ByteUtil.toHexString(friendPk);
+                logger.trace("onReadMessageRoot friendPk::{}，MessageRoot::{}",
+                        friendPkStr, hash);
+                String userPk = MainApplication.getInstance().getPublicKey();
+                ChatMsgLog msgLog = new ChatMsgLog(hash, userPk, friendPkStr,
+                        ChatMsgStatus.RECEIVED_CONFIRMATION.getStatus(), DateUtil.getMillisTime());
+                long result = chatRepo.addChatMsgLog(msgLog);
+                logger.trace("updateReceivedConfirmationState friendPk::{}, msgRoot::{}, result::{}",
+                        friendPkStr, hash, result);
+            } catch (SQLiteConstraintException ignore) {
+            } catch (Exception e) {
+                logger.error("onReadMessageRoot error", e);
+            }
+            emitter.onComplete();
+        }, BackpressureStrategy.LATEST)
+                .subscribeOn(Schedulers.io())
+                .subscribe();
+        disposables.add(disposable);
     }
 
     /**
@@ -138,34 +165,40 @@ class MsgListenHandler extends MsgListener{
      */
     @Override
     public void onDiscoveryFriend(byte[] friendPk, BigInteger timestamp) {
-        logger.debug("onDiscoveryFriend friendPk::{}",
-                ByteUtil.toHexString(friendPk));
-        try {
-            String userPk = MainApplication.getInstance().getPublicKey();
-            String friendPkStr = ByteUtil.toHexString(friendPk);
-            Friend friend = friendRepo.queryFriend(userPk, friendPkStr);
-            long latestOnlineTime = timestamp.longValue();
-            if (friend != null) {
-                boolean isUpdate = false;
-                if (friend.state != 2) {
-                    friend.state = 2;
-                    isUpdate = true;
+        Disposable disposable = Flowable.create(emitter -> {
+            logger.debug("onDiscoveryFriend friendPk::{}",
+                    ByteUtil.toHexString(friendPk));
+            try {
+                String userPk = MainApplication.getInstance().getPublicKey();
+                String friendPkStr = ByteUtil.toHexString(friendPk);
+                Friend friend = friendRepo.queryFriend(userPk, friendPkStr);
+                long latestOnlineTime = timestamp.longValue();
+                if (friend != null) {
+                    boolean isUpdate = false;
+                    if (friend.state != 2) {
+                        friend.state = 2;
+                        isUpdate = true;
+                    }
+                    // 当前时间大于上次更新时间
+                    if (latestOnlineTime > friend.lastSeenTime) {
+                        friend.lastSeenTime = latestOnlineTime;
+                        isUpdate = true;
+                    }
+                    if (isUpdate) {
+                        friendRepo.updateFriend(friend);
+                    }
+                    logger.info("onDiscoveryFriend friendPk::{}, timestamp::{}",
+                            ByteUtil.toHexString(friendPk),
+                            DateUtil.formatTime(latestOnlineTime, DateUtil.pattern6));
                 }
-                // 当前时间大于上次更新时间
-                if (latestOnlineTime > friend.lastSeenTime) {
-                    friend.lastSeenTime = latestOnlineTime;
-                    isUpdate = true;
-                }
-                if (isUpdate) {
-                    friendRepo.updateFriend(friend);
-                }
-                logger.info("onDiscoveryFriend friendPk::{}, timestamp::{}",
-                        ByteUtil.toHexString(friendPk),
-                        DateUtil.formatTime(latestOnlineTime, DateUtil.pattern6));
+            } catch (Exception e) {
+                logger.error("onDiscoveryFriend error", e);
             }
-        } catch (Exception e) {
-            logger.error("onDiscoveryFriend error", e);
-        }
+            emitter.onComplete();
+        }, BackpressureStrategy.LATEST)
+                .subscribeOn(Schedulers.io())
+                .subscribe();
+        disposables.add(disposable);
     }
 
     /**
@@ -176,23 +209,29 @@ class MsgListenHandler extends MsgListener{
      */
     @Override
     public void onMessageStatus(byte[] friend, byte[] root, MsgStatus msgStatus) {
-        try {
-            String hash = ByteUtil.toHexString(root);
-            logger.trace("onMessageStatus root::{}, msgStatus::{}",
-                    hash, msgStatus.name());
-            if (msgStatus.ordinal() == MsgStatus.PUT_SUCCESS.ordinal()) {
-                logger.trace("onMessageStatus put success hash::{}",
-                        hash);
+        Disposable disposable = Flowable.create(emitter -> {
+            try {
+                String hash = ByteUtil.toHexString(root);
+                logger.trace("onMessageStatus root::{}, msgStatus::{}",
+                        hash, msgStatus.name());
+                if (msgStatus.ordinal() == MsgStatus.PUT_SUCCESS.ordinal()) {
+                    logger.trace("onMessageStatus put success hash::{}",
+                            hash);
+                }
+                int status = msgStatus.ordinal();
+                String userPk = MainApplication.getInstance().getPublicKey();
+                String friendPk = ByteUtil.toHexString(friend);
+                ChatMsgLog msgLog = new ChatMsgLog(hash, userPk, friendPk, status,
+                        DateUtil.getMillisTime());
+                chatRepo.addChatMsgLog(msgLog);
+            } catch (Exception e) {
+                logger.error("onMessageStatus error", e);
             }
-            int status = msgStatus.ordinal();
-            String userPk = MainApplication.getInstance().getPublicKey();
-            String friendPk = ByteUtil.toHexString(friend);
-            ChatMsgLog msgLog = new ChatMsgLog(hash, userPk, friendPk, status,
-                    DateUtil.getMillisTime());
-            chatRepo.addChatMsgLog(msgLog);
-        } catch (Exception e) {
-            logger.error("onMessageStatus error", e);
-        }
+            emitter.onComplete();
+        }, BackpressureStrategy.LATEST)
+                .subscribeOn(Schedulers.io())
+                .subscribe();
+        disposables.add(disposable);
     }
 
     /**
@@ -201,16 +240,22 @@ class MsgListenHandler extends MsgListener{
      */
     @Override
     public void onNewDeviceID(byte[] deviceID) {
-        try {
-            String userPk = MainApplication.getInstance().getPublicKey();
-            String deviceIDStr = new String(deviceID);
-            logger.debug("onNewDeviceID userPk::{}, deviceID::{}",
-                    userPk, deviceIDStr);
-            Device device = new Device(userPk, deviceIDStr, DateUtil.getTime());
-            deviceRepo.addDevice(device);
-        } catch (Exception e) {
-            logger.error("onNewDeviceID error", e);
-        }
+        Disposable disposable = Flowable.create(emitter -> {
+            try {
+                String userPk = MainApplication.getInstance().getPublicKey();
+                String deviceIDStr = new String(deviceID);
+                logger.debug("onNewDeviceID userPk::{}, deviceID::{}",
+                        userPk, deviceIDStr);
+                Device device = new Device(userPk, deviceIDStr, DateUtil.getTime());
+                deviceRepo.addDevice(device);
+            } catch (Exception e) {
+                logger.error("onNewDeviceID error", e);
+            }
+            emitter.onComplete();
+        }, BackpressureStrategy.LATEST)
+                .subscribeOn(Schedulers.io())
+                .subscribe();
+        disposables.add(disposable);
     }
 
     /**
@@ -219,30 +264,36 @@ class MsgListenHandler extends MsgListener{
      */
     @Override
     public void onNewFriendFromMultiDevice(byte[] friendPk) {
-        try {
-            String userPk = MainApplication.getInstance().getPublicKey();
-            String friendPkStr = ByteUtil.toHexString(friendPk);
-            logger.debug("onNewFriend userPk::{}, friendPk::{}",
-                    userPk, friendPkStr);
-            User user = userRepo.getUserByPublicKey(friendPkStr);
-            if (null == user) {
-                user = new User(friendPkStr);
-                userRepo.addUser(user);
-            }
-            if (StringUtil.isNotEquals(userPk, friendPkStr)) {
-                Friend friend = friendRepo.queryFriend(userPk, friendPkStr);
-                if (friend != null) {
-                    if (friend.state == 0) {
-                        friend.state = 1;
-                        friendRepo.updateFriend(friend);
-                    }
-                } else {
-                    friend = new Friend(userPk, friendPkStr, 1);
-                    friendRepo.addFriend(friend);
+        Disposable disposable = Flowable.create(emitter -> {
+            try {
+                String userPk = MainApplication.getInstance().getPublicKey();
+                String friendPkStr = ByteUtil.toHexString(friendPk);
+                logger.debug("onNewFriend userPk::{}, friendPk::{}",
+                        userPk, friendPkStr);
+                User user = userRepo.getUserByPublicKey(friendPkStr);
+                if (null == user) {
+                    user = new User(friendPkStr);
+                    userRepo.addUser(user);
                 }
+                if (StringUtil.isNotEquals(userPk, friendPkStr)) {
+                    Friend friend = friendRepo.queryFriend(userPk, friendPkStr);
+                    if (friend != null) {
+                        if (friend.state == 0) {
+                            friend.state = 1;
+                            friendRepo.updateFriend(friend);
+                        }
+                    } else {
+                        friend = new Friend(userPk, friendPkStr, 1);
+                        friendRepo.addFriend(friend);
+                    }
+                }
+            } catch (Exception e) {
+                logger.error("onNewFriend error", e);
             }
-        } catch (Exception e) {
-            logger.error("onNewFriend error", e);
-        }
+            emitter.onComplete();
+        }, BackpressureStrategy.LATEST)
+                .subscribeOn(Schedulers.io())
+                .subscribe();
+        disposables.add(disposable);
     }
 }
